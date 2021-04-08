42c1e5f67daa511fd80e2951f3cd19d0ccc61de2
fusemasahiro <konanntolupan@gmail.com>
2021-04-08 10:19:11 +0900

    スライド

--- js/main.js
+++ js/main.js
@@ -8,11 +8,30 @@
 }
 
 {
-  const img_src = ["img/DSC_1381.JPG", "img/DSC_1401.JPG", "img/DSC_1417.JPG", "img/DSC_1408.JPG"];
+  const images = [
+    'img/1502213032650.jpg', 
+    'img/20201007_121647.jpg'
+  ]
+  let num = -1;
+
+  document.getElementById('main').addEventListener('click', () => {
+    const mainImage = document.getElementById('main');
+    if(num === 1) { 
+      num = 0;
+    } else {
+      num ++;
+    }
+    mainImage.src = images[num];
+  });
+
+}
+
+{
+  const img_src = ["img/DSC_1381.JPG", "img/DSC_1083.JPG", "img/DSC_1401.JPG", "img/DSC_1417.JPG", "img/DSC_1408.JPG"];
   let num = -1;
 
   function slide_time() {
-    if(num === 3) {
+    if(num === 4) {
       num = 0;
     } else {
       num ++;
